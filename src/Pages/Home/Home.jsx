095616d00d5240ba19a8c008043c112054cf26b8
Himesh Hotwani <heemarco@gmail.com>
2023-12-23 14:29:55 +0530

    added api but not rendering

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Home = () => {
+function Home() {
+  const [executionresult, setExecutionResult] = useState('');
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/run-python');
+      setExecutionResult(response.data.success ? response.data.output : response.data.error);
+    } catch (error) {
+      console.error('Error running Python code:', error);
+    }
+  };
+
   return (
     <div>
-      <h1>This is Home page</h1>
-      <button type='button'>Start</button>
+      <h1>Meet Link</h1>
+      <button onClick={handleButtonClick}>Run Python Code</button>
+      <div>
+        <strong>Result:</strong>
+        <pre>{executionresult}</pre>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
